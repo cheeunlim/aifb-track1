306cf07a88961427e27d8831063eb85e11fa8291
jk1333 <jk13@nate.com>
2026-07-13 16:23:06 +0900

    session2: fix provisioning script

--- session2_index_builder.py
+++ session2_index_builder.py
@@ -1,13 +1,15 @@
 import google.auth
+import time
 
 _, PROJECT_ID = google.auth.default()
 LOCATION="asia-northeast1"
 COLLECTION_ID="amazon-product-768-compact"
 
-from google.cloud import vectorsearch_v1beta
+from datetime import datetime
+from google.cloud import vectorsearch_v1
 
 # Create the client
-vector_search_service_client = vectorsearch_v1beta.VectorSearchServiceClient()
+vector_search_service_client = vectorsearch_v1.VectorSearchServiceClient()
 
 # The JSON schema for the data
 data_schema = {
@@ -24,11 +26,11 @@ vector_schema = {
     "text_embedding": {"dense_vector": {"dimensions": 768}}
 }
 
-collection = vectorsearch_v1beta.Collection(
+collection = vectorsearch_v1.Collection(
     data_schema=data_schema,
     vector_schema=vector_schema,
 )
-request = vectorsearch_v1beta.CreateCollectionRequest(
+request = vectorsearch_v1.CreateCollectionRequest(
     parent=f"projects/{PROJECT_ID}/locations/{LOCATION}",
     collection_id=COLLECTION_ID,
     collection=collection,
@@ -38,13 +40,8 @@ request = vectorsearch_v1beta.CreateCollectionRequest(
 operation = vector_search_service_client.create_collection(request=request)
 
 # Wait for the result (note this may take up to several minutes)
+time.sleep(30)
 operation.result()
-
-from datetime import datetime
-from google.cloud import vectorsearch_v1
-
-# Create the client
-vector_search_service_client = vectorsearch_v1.VectorSearchServiceClient()
 
 # Initialize request
 request = vectorsearch_v1.ImportDataObjectsRequest(
@@ -59,25 +56,20 @@ request = vectorsearch_v1.ImportDataObjectsRequest(
 print(datetime.now()) 
 operation = vector_search_service_client.import_data_objects(request=request)
 
-import time
+time.sleep(30)
 while operation.done() == False:
     time.sleep(1)
 print(f"Collection created at {datetime.now()}")
 time.sleep(30)
 
-from google.cloud import vectorsearch_v1beta
-
-def create_index(index_field: str):
-    # Create a client
-    client = vectorsearch_v1beta.VectorSearchServiceClient()
-
+def create_index(client, index_field: str):
     # Initialize request argument(s)
-    index = vectorsearch_v1beta.Index(
+    index = vectorsearch_v1.Index(
         index_field=index_field,
         #filter_fields=["year", "genre"],
         store_fields=["name", "description"],
     )
-    request = vectorsearch_v1beta.CreateIndexRequest(
+    request = vectorsearch_v1.CreateIndexRequest(
         parent=f"projects/{PROJECT_ID}/locations/{LOCATION}/collections/{COLLECTION_ID}",
         index_id=f"idx-{index_field.replace('_', '-')}",
         index=index,
@@ -86,14 +78,15 @@ def create_index(index_field: str):
     # Make the request
     return client.create_index(request=request)
 
-
-operation = create_index("text_embedding")
+operation = create_index(vector_search_service_client, "text_embedding")
+time.sleep(30)
 while operation.done() == False:
     time.sleep(1)
 print(f"Text embedding index created at {datetime.now()}")
 time.sleep(30)
 
-operation = create_index("image_embedding")
+operation = create_index(vector_search_service_client, "image_embedding")
+time.sleep(30)
 while operation.done() == False:
     time.sleep(1)
 print(f"Image embedding index created at {datetime.now()}")
